1fd316ae0c23246a11851401cf5c2575c1c13571
Thomas Micol <thomasmicol@gmail.com>
2020-10-21 21:08:07 +1300

    Added instantiation function files for the DI  looping

---
+++ IoC/DependencyInjectionRegistration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Blazor_With_DI_and_Looping_Registration.IoC
+{
+    public static class DependencyInjectionRegistration
+    {
+        public static void Register(IServiceCollection services)
+        {
+
+        }
+    }
+}
---
+++ IoC/InstantiationFunctions/IInstantiationFunction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor_With_DI_and_Looping_Registration.IoC.InstantiationFunctions
+{
+    interface IInstantiationFunction
+    {
+        void Register()
+    }
+}
