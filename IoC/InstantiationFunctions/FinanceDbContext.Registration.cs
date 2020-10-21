f904abe7755667213e61b42995b309dd688b8ed0
Thomas Micol <thomasmicol@gmail.com>
2020-10-21 21:59:51 +1300

    added db context registration

---
+++ IoC/InstantiationFunctions/FinanceDbContext.Registration.cs
@@ -0,0 +1,17 @@
+﻿using Blazor_With_DI_and_Looping_Registration.Data;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor_With_DI_and_Looping_Registration.IoC.InstantiationFunctions
+{
+    public class FinanceDbContextRegistration : IInstantiationFunction
+    {
+        public void Register(IServiceCollection services)
+        {
+            services.AddScoped<FinanceDbContext>();
+        }
+    }
+}
