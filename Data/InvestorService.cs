689b5ea812a22e7517e8abbe2e2d468ed3f5c4ef
Thomas Micol <thomasmicol@gmail.com>
2020-10-21 22:02:49 +1300

    Added an investor service used in the fetch data page.

---
+++ Data/InvestorService.cs
@@ -0,0 +1,33 @@
+﻿using Blazor_With_DI_and_Looping_Registration.Data.Data_Set_Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor_With_DI_and_Looping_Registration.Data
+{
+    public class InvestorService
+    {
+        public InvestorService(FinanceDbContext dbContext)
+        {
+            DbContext = dbContext;
+        }
+
+        public FinanceDbContext DbContext { get; }
+
+        public Task<Investor> GetInvestor(long investorId)
+        {
+            return Task.FromResult(DbContext.Investors.Where(i => i.Id == investorId).FirstOrDefault());
+        }
+
+        public void CreateInvestor(Investor investor)
+        {
+            if (DbContext.Investors.Contains(investor))
+            {
+                return;
+            }
+            DbContext.Investors.Add(investor);
+            DbContext.SaveChanges();
+        }
+    }
+}
