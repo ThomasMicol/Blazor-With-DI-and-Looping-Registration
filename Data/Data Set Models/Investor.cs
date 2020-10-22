95b28c0b91546e85a78c4564dbd6e2396b9fae47
Thomas Micol <thomasmicol@gmail.com>
2020-10-22 23:02:41 +1300

    Updated the investor dbset model to include the login information

--- Data/Data Set Models/Investor.cs
+++ Data/Data Set Models/Investor.cs
@@ -6,6 +6,12 @@ namespace Blazor_With_DI_and_Looping_Registration.Data.Data_Set_Models
     {
         [Required]
         public long Id { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+
         public string Name { get; set; }
         public decimal Income { get; set; }
     }
