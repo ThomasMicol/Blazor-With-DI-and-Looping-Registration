18e980bece4ff9d2fe20e87e056eb76566294ed9
Thomas Micol <thomasmicol@gmail.com>
2020-10-25 21:02:08 +1300

    added extension method for the formatting of currency

---
+++ Data/Extensions/DecimalExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor_With_DI_and_Looping_Registration.Data.Extensions
+{
+    public static class DecimalExtensions
+    {
+        public static string ToCurrency(this decimal dec)
+        {
+            var stringDec = dec.ToString();
+            if((dec % 1) == 0)
+            {
+                stringDec = $"{stringDec}.00";
+            }
+
+            return $"${stringDec}";
+        }
+    }
+}
