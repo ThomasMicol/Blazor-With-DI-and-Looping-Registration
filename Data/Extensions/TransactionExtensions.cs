fa3a9197c6381e53cc96a669ccb6a0fc80fcb6b8
Thomas Micol <thomasmicol@gmail.com>
2020-10-26 02:19:55 +1300

    added extension method for convering a transaction into a standard time frame

---
+++ Data/Extensions/TransactionExtensions.cs
@@ -0,0 +1,46 @@
+﻿using Blazor_With_DI_and_Looping_Registration.Data.Data_Set_Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor_With_DI_and_Looping_Registration.Data.Extensions
+{
+    public static class TransactionExtensions
+    {
+        public static IncomeTransaction ConvertToYearlyStandard(this IncomeTransaction transaction)
+        {
+            switch (transaction.Frequency)
+            {
+                case TransactionFrequency.Weekly:
+                    return new IncomeTransaction
+                    {
+                        Id = transaction.Id,
+                        Investor = transaction.Investor,
+                        Frequency = TransactionFrequency.Yearly,
+                        TransactionAmount = transaction.TransactionAmount * 52
+                    };
+                case TransactionFrequency.Fortnightly:
+                    return new IncomeTransaction
+                    {
+                        Id = transaction.Id,
+                        Investor = transaction.Investor,
+                        Frequency = TransactionFrequency.Yearly,
+                        TransactionAmount = transaction.TransactionAmount * 26
+                    };
+                case TransactionFrequency.Monthly:
+                    return new IncomeTransaction
+                    {
+                        Id = transaction.Id,
+                        Investor = transaction.Investor,
+                        Frequency = TransactionFrequency.Yearly,
+                        TransactionAmount = transaction.TransactionAmount * 12
+                    };
+                case TransactionFrequency.Yearly:
+                    return transaction;
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+    }
+}
