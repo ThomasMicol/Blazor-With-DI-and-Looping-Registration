f0436d1b76aa90cd5d9f40bb9a4abc62bd6b37ce
Thomas Micol <thomasmicol@gmail.com>
2020-11-06 23:43:42 +1300

    Added the payment form model

---
+++ Data/Dashboard/Models/PaymentForm.cs
@@ -0,0 +1,15 @@
+﻿using Blazor_With_DI_and_Looping_Registration.Data.Data_Set_Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Blazor_With_DI_and_Looping_Registration.Data.Dashboard.Models
+{
+    public class PaymentForm
+    {
+        public decimal Amount { get; set; }
+        public TransactionFrequency Frequency { get; set; }
+    }
+}
