c6091c2f7388a7540b500d05bfd352c5462ac9a7
Thomas Micol <thomasmicol@gmail.com>
2020-11-06 23:43:58 +1300

    added the dashboard service and related interface

---
+++ Data/Dashboard/IDashboardService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor_With_DI_and_Looping_Registration.Data.Dashboard
+{
+    interface IDashboardService
+    {
+    }
+}
