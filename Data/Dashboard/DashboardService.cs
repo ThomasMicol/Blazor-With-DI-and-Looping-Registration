d08ee0f29e9b2c84735d689ceab45a05d8a058e7
Thomas Micol <thomasmicol@gmail.com>
2020-11-08 15:19:21 +1300

    wrote implementation for the dashboard service

--- Data/Dashboard/DashboardService.cs
+++ Data/Dashboard/DashboardService.cs
@@ -1,11 +1,51 @@
-﻿using System;
+﻿using Blazor_With_DI_and_Looping_Registration.Data.Dashboard.Models;
+using Blazor_With_DI_and_Looping_Registration.Data.Data_Set_Models;
+using Blazor_With_DI_and_Looping_Registration.Data.Login;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Blazor_With_DI_and_Looping_Registration.Data.Dashboard
 {
-    public class DashboardService
+    public class DashboardService : IDashboardService
     {
+        public FinanceDbContext DbContext { get; }
+        public ILoginService LoginService { get; }
+
+        public DashboardService(FinanceDbContext dbContext
+            , ILoginService loginService)
+        {
+            DbContext = dbContext;
+            LoginService = loginService;
+        }
+
+        public void AddExpense(PaymentForm formData)
+        {
+            var expenseData = new ExpenseTransaction
+            {
+                Id = Guid.NewGuid(),
+                Frequency = formData.Frequency,
+                Investor = LoginService.Investor,
+                TransactionAmount = formData.Amount,
+                TransactionType = TransactionType.Expense
+            };
+            DbContext.Add<ExpenseTransaction>(expenseData);
+            DbContext.SaveChanges();
+        }
+
+        public void AddIncome(PaymentForm formData)
+        {
+            var incomeData = new IncomeTransaction
+            {
+                Id = Guid.NewGuid(),
+                Frequency = formData.Frequency,
+                Investor = LoginService.Investor,
+                TransactionAmount = formData.Amount,
+                TransactionType = TransactionType.Income
+            };
+            DbContext.Add<IncomeTransaction>(incomeData);
+            DbContext.SaveChanges();
+        }
     }
 }
