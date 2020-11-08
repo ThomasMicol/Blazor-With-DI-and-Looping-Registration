55f55019255063540d1a96a3c6cfe089bbaacce1
Thomas Micol <thomasmicol@gmail.com>
2020-11-08 13:37:15 +1300

    added dashboard service registration in the ioc

---
+++ IoC/InstantiationFunctions/DashboardService.Registration.cs
@@ -0,0 +1,17 @@
+﻿using Blazor_With_DI_and_Looping_Registration.Data.Dashboard;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor_With_DI_and_Looping_Registration.IoC.InstantiationFunctions
+{
+    public class DashboardServiceRegistration : IInstantiationFunction
+    {
+        public void Register(IServiceCollection services)
+        {
+            services.AddScoped<IDashboardService, DashboardService>();
+        }
+    }
+}
