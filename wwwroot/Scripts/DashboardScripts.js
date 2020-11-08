f7206af32ffcf1613cffd810e9d4176469183a3d
Thomas Micol <thomasmicol@gmail.com>
2020-11-08 15:22:34 +1300

    wrote some javascript dashboard methods for interacting with dynamic elements such as the counter and modals

--- wwwroot/Scripts/DashboardScripts.js
+++ wwwroot/Scripts/DashboardScripts.js
@@ -8,3 +8,18 @@ window.openAddExpenseModalElement = function () {
 
     modal.show();
 }
+
+window.closeAddIncomeModalElement = function () {
+    var modal = document.querySelector("#addIncomeModal");
+    modal.close();
+}
+
+window.closeAddExpenseModalElement = function () {
+    var modal = document.querySelector("#addExpenseModal");
+    modal.close();
+}
+
+window.updateCounter = function (args) {
+    var counter = document.querySelector(args[0]);
+    counter.update(args[1]);
+}
