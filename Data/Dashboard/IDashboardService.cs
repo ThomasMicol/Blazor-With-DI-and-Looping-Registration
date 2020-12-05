09b09757a67a1b30186c9654c0ecaddee94c28fa
Thomas Micol <thomasmicol@gmail.com>
2020-12-05 18:15:06 +1300

    simplified names in the dashboard service

--- Data/Dashboard/IDashboardService.cs
+++ Data/Dashboard/IDashboardService.cs
@@ -7,7 +7,7 @@ namespace Blazor_With_DI_and_Looping_Registration.Data.Dashboard
 {
     interface IDashboardService
     {
-        void AddIncome(Blazor_With_DI_and_Looping_Registration.Data.Dashboard.Models.PaymentForm formData);
-        void AddExpense(Blazor_With_DI_and_Looping_Registration.Data.Dashboard.Models.PaymentForm formData);
+        void AddIncome(Models.PaymentForm formData);
+        void AddExpense(Models.PaymentForm formData);
     }
 }
